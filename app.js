e96162dfcabb89b70c8b9a8d322e2e83a5a96982
TriDiamond <code.tridiamond@gmail.com>
2021-07-08 09:30:02 +0800

    Adding in `express-session`.

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const path = require('path')
 const mongoose = require('mongoose')
 const morgan = require('morgan')
 const ejsMate = require('ejs-mate')
+const session = require('express-session')
+
 const methodOverride = require('method-override')
 const ExpressError = require('./utils/ExpressError')
 
@@ -34,6 +36,18 @@ app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 app.use(morgan('dev'))
 app.use(express.static('public')) // Adding public resources
+
+const sessionConfig = {
+  secret: 'this_should_be_a_better_secret!',
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    httpOnly: true,
+    expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+    maxAge: 1000 * 60 * 60 * 24 * 7
+  }
+}
+app.use(session(sessionConfig))
 
 // Routers
 app.use('/campgrounds', campgroundsRoutes)
