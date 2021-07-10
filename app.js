8148de86523abec23c3ec407dd08cd8cfd0df443
TriDiamond <code.tridiamond@gmail.com>
2021-07-10 16:34:00 +0800

    Adding start script.

--- app.js
+++ app.js
@@ -170,6 +170,8 @@ app.use((err, req, res, next) => {
   res.status(status).render('error', { err })
 })
 
-app.listen(3000, () => {
-  console.log('Serving on port 3000...')
+const port = process.env.PORT || 3000
+
+app.listen(port, () => {
+  console.log(`App is running on port ${port}`)
 })
